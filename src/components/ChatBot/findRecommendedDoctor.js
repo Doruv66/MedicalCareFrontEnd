444cbb31e9474dc80bb1d0b3d015b11d732f6c8f
Dorian Vieru <d.vieru@student.fontys.nl>
2024-01-19 22:42:27 +0100

    docker up and running and imporoved the chat bot

--- src/components/ChatBot/findRecommendedDoctor.js
+++ src/components/ChatBot/findRecommendedDoctor.js
@@ -1,9 +1,27 @@
 
 const specialtyMap = {
     'chest pain': 'Cardiology',
-    'rash': 'Dermatologist',
-    'joint pain': 'Rheumatologist',
-    // Add more symptoms and specialties as needed
+    'menstrual irregularities': 'Gynecology',
+    'rash': 'Dermatology',
+    'unexplained weight loss': 'Oncology',
+    'headaches': 'Neurology',
+    'back pain': 'Orthopedics',
+    'mood disorders': 'Psychiatry',
+    'fatigue': 'Internal Medicine',
+    'fever in children': 'Pediatrics',
+    'chest pain': 'Cardiology',
+    'skin changes': 'Dermatology',
+    'hair loss': 'Dermatology',
+    'sexual health concerns': 'Gynecology',
+    'eating disorders': 'Psychiatry',
+    'bone fractures': 'Orthopedics',
+    'dizziness and balance issues': 'Neurology',
+    'sleep disorders': 'Sleep Medicine',
+    'skin cancer': 'Dermatology',
+    'joint swelling': 'Orthopedics',
+    'high blood pressure': 'Cardiology',
+    'diabetes management': 'Endocrinology',
+    'pediatric developmental issues': 'Pediatrics',
   };
 
 const findRecommendeDoctor = (value) => {
